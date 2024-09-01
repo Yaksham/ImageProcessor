a840e97374cc936ac6b2ae10e831a438df07de72
Tejasv Plawat <82596143+Yaksham@users.noreply.github.com>
2024-09-01 12:01:48 +0530

    Update db.py

--- app/db.py
+++ app/db.py
@@ -15,7 +15,9 @@ engine = create_async_engine(DATABASE_URL, echo=True, future=True)
 Base = declarative_base()
 
 # create tables
-Base.metadata.create_all(bind=engine)
+async def create_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
 
 async_session = sessionmaker(
     bind=engine,
